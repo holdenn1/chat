3ed95af0998b10f5f58295829e156c68ec5ca76e
Ihor Ivliev <igor852745@gmail.com>
2020-05-29 23:34:03 +0300

    join formik

--- src/modules/RegisterForm/components/RegisterForm.jsx
+++ src/modules/RegisterForm/components/RegisterForm.jsx
@@ -1,72 +1,93 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom'
 import {Block, Button} from "../../../components";
 import {Form, Input} from 'antd';
 import {MailOutlined, LockOutlined, InfoCircleTwoTone} from '@ant-design/icons';
 
-class RegisterForm extends Component {
+const RegisterForm = props => {
+  const success = false;
+  const {
+    values,
+    touched,
+    errors,
+    handleChange,
+    handleBlur,
+    handleSubmit,
 
-  render () {
-    const success = false;
-    return (
 
-       <div>
-             <div className="auth__top">
-               <h2>Регистрация</h2>
-               <p>Для входа в чат, вам нужно зарегистрироваться</p>
-             </div>
-         <Block>
-           {!success ?(
-              <Form name="normal_login"
-                    className="login-form">
-                <Form.Item validateStatus='success'
-                           hasFeedback>
-                  <Input prefix={<MailOutlined className="site-form-item-icon"/>}
-                         type='email'
-                         placeholder="Email"
-                         size='large'/>
-                </Form.Item>
-                <Form.Item>
-                  <Input
-                     prefix={<LockOutlined className="site-form-item-icon"/>}
-                     type="user"
-                     placeholder="Ваше имя"
-                     size='large'/>
-                </Form.Item>
-                <Form.Item>
-                  <Input
-                     prefix={<LockOutlined className="site-form-item-icon"/>}
-                     type="password"
-                     placeholder="Пароль"
-                     size='large'/>
-                </Form.Item>
-                <Form.Item>
-                  <Input
-                     prefix={<LockOutlined className="site-form-item-icon"/>}
-                     type="password"
-                     placeholder="Повторите пароль"
-                     size='large'/>
-                </Form.Item>
-                <Form.Item>
-                  <Button type="primary" size='large'>
-                    Зарегестрироваться
-                  </Button>
-                </Form.Item>
-                <Link className='auth register-link' to='/'>Войти в аккаунт</Link>
-              </Form>
-           ): (<div className='auth__success-block'>
-             <div>
-               <InfoCircleTwoTone style={{fontSize: '50px'}} type='info-circle' theme='twoTone'/>
-             </div>
-             <h3>Подтвердите свой аккаунт</h3>
-             <p>На вашу почту отправлено письмо с сылкой на подтверждение аккаунта.</p>
-           </div>)}
+  } = props;
+  return (
 
-         </Block>
+     <div>
+       <div className="auth__top">
+         <h2>Регистрация</h2>
+         <p>Для входа в чат, вам нужно зарегистрироваться</p>
        </div>
+       <Block>
+         {!success ? (
+            <Form onSubmit={handleSubmit}
+                  className="login-form">
+              <Form.Item validateStatus={!touched.email ? '' : errors.email ? 'error' : 'success'}
 
-    )
-  }
+                         hasFeedback>
+                <Input
+                   id='email'
+                   prefix={<MailOutlined className="site-form-item-icon"/>}
+                   type='email'
+                   placeholder="Email"
+                   value={values.email}
+                   onChange={handleChange}
+                   onBlur={handleBlur}
+                   size='large'/>
+              </Form.Item>
+              <Form.Item>
+                <Input
+                   prefix={<LockOutlined className="site-form-item-icon"/>}
+                   type="user"
+                   placeholder="Ваше имя"
+                   size='large'/>
+              </Form.Item>
+              <Form.Item validateStatus={
+                !touched.password ? '' : errors.password ? 'error' : 'success'
+              }
+                         hasFeedback
+              >
+                <Input
+                   id='password'
+                   prefix={<LockOutlined className="site-form-item-icon"/>}
+                   type="password"
+                   value={values.password}
+                   onChange={handleChange}
+                   onBlur={handleBlur}
+                   placeholder="Пароль"
+                   size='large'/>
+              </Form.Item>
+              <Form.Item>
+                <Input
+                   prefix={<LockOutlined className="site-form-item-icon"/>}
+                   type="password"
+                   placeholder="Повторите пароль"
+                   size='large'/>
+              </Form.Item>
+              <Form.Item>
+                <Button onClick={handleSubmit} type="primary" size='large'>
+                  Зарегестрироваться
+                </Button>
+              </Form.Item>
+              <Link className='auth register-link' to='/'>Войти в аккаунт</Link>
+            </Form>
+         ) : (<div className='auth__success-block'>
+           <div>
+             <InfoCircleTwoTone style={{fontSize: '50px'}} type='info-circle' theme='twoTone'/>
+           </div>
+           <h3>Подтвердите свой аккаунт</h3>
+           <p>На вашу почту отправлено письмо с сылкой на подтверждение аккаунта.</p>
+         </div>)}
+
+       </Block>
+     </div>
+
+  )
 }
 
 export default RegisterForm
--- src/modules/RegisterForm/containers/RegisterForm.js
+++ src/modules/RegisterForm/containers/RegisterForm.js
@@ -1,3 +1,34 @@
+import {withFormik} from 'formik'
 import RegisterForm from '../components/RegisterForm'
 
-export default RegisterForm
+export default withFormik({
+  validate: values => {
+    let errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (
+       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(
+          values.email
+       )
+    ) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+      errors.password = 'Input password'
+    } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/i.test(values.password)) {
+      errors.password = 'Password not valid'
+    }
+    return errors;
+  },
+
+
+  handleSubmit: (values, {setSubmitting}) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  },
+
+  displayName: 'RegisterForm',
+})(RegisterForm);
